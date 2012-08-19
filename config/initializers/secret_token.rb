47cd305751df2773ab9943f7fe5d6bd46b4b21ba
Andrew Svoboda <svoboda.andrew@gmail.com>
2012-08-19 13:48:07 -0400

    updating parts of the site. UI and everything

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PsychicOctoHipster::Application.config.secret_token = '81e5ed62ad1a71d48a1bbb91da3618b24ca87e67abac3ad6dc69aeea81d4c613637986f44bf7975209ae7e5c686ee69b74fed219eb6c12d46603d61f1d0d3c9c'
