5ee73f57fc4909bc1e2f6235558b7f9456712fd9
Andrew Svoboda <svoboda.andrew@gmail.com>
2012-11-19 14:37:54 -0500

    front page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,8 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
-
+    @posts = Post.paginate(:page => params[:page], :per_page => 10)
+	
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @posts }
