1ffde41016476bb092ed88093c76d5fad01c2e9d
Andrew Svoboda <svoboda.andrew@gmail.com>
2012-12-07 13:26:36 -0500

    slightly more changes muaahahah

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.paginate(:page => params[:page], :per_page => 10, :order => "created_at DESC")
+    @posts = Post.paginate(:page => params[:page], :per_page => 5, :order => "created_at DESC")
 
     respond_to do |format|
       format.html # index.html.erb
