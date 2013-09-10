2c9709a6b2c1e147ed680fc81597d4bb2b40e2bc
Andrew Svoboda <asvoboda@uwaterloo.ca>
2013-09-09 21:50:14 -0400

    added covers to gallery page

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -1,6 +1,7 @@
 class GalleryController < ApplicationController
   def index
 	@posts = Post.where("PHOTO_FILE_NAME IS NOT NULL").order('created_at DESC')
+	@covers = Cover.all()
 	respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @posts }
