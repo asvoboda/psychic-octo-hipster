df41c85144df2d22a295426bb833c755f178b5c5
Andrew Svoboda <asvoboda@uwaterloo.ca>
2013-09-16 00:07:58 -0400

    fixed gallery page. maybe. not sure if still borked though.

--- app/assets/javascripts/utils.js
+++ app/assets/javascripts/utils.js
@@ -14,22 +14,20 @@ var linear_partition = function (seq, k) {
 		});
 	}
 	
-	var table = new Array(k);
-	for (var i = 0; i < k; i++) {
-		table[i] = new Array(n);
-	}
-	
-	var solution = new Array(k-1);
-	for (var i = 0; i < k-1; i++) {
-		table[i] = new Array(n-1);
-	}
-	
-	for(var i=0; i < n; i++) {
-		var temp = 0;
-		if(i) {
-			temp = table[i-1][0];
+	var table = new Array(n);
+	for (var i = 0; i < n; i++) {
+		table[i] = new Array(k);
+		for(var j = 0; j < k; j ++) {
+			table[i][j] = 0;
 		}
-		table[i][0] = seq[i] + temp;
+	}
+	
+	var solution = new Array(n-1);
+	for (var i = 0; i < n-1; i++) {
+		solution[i] = new Array(k-1);
+		for(var j = 0; j < k-1; j ++) {
+			solution[i][j] = 0;
+		}		
 	}
 	
 	for(var i=0; i < n; i++) {
@@ -51,7 +49,9 @@ var linear_partition = function (seq, k) {
 			for(var x = 0; x < i; x++) {
 				temp_m[x] = [_.max([table[x][j-1], table[i][0]-table[x][0]]) , x];
 			}
-			m = _.min(temp_m);
+			m = _.min(temp_m, function(o) {
+				return o[0];
+			});
 			table[i][j] = m[0];
 			solution[i-1][j-1] = m[1];
 		}
@@ -61,6 +61,7 @@ var linear_partition = function (seq, k) {
 	k -= 2;
 	var ans = [];
 	while (k >= 0) {
+		temp_ans = new Array(n+1);
 		for(var i = solution[n-1][k]+1; i < n+1; i++) {
 			temp_ans[i] = seq[i];
 		}
@@ -68,12 +69,12 @@ var linear_partition = function (seq, k) {
 		n = solution[n-1][k];
 		k--;
 	}
-	
-	var ret;
+
+	var ret = [];
 	for(var i = 0; i < n+1; i++) {
-		ret = seq[i].concat(ans);
+		ret.push(seq[i]);
 	}
-	return ret;
+	return ret.concat(ans);
 };
 
 var resize_img = function($p, ideal_width, ideal_height) {
@@ -83,10 +84,12 @@ var resize_img = function($p, ideal_width, ideal_height) {
 var galleryify = function() {
 	var $imgs = $("#gallery .picture img");
 	var viewport_width = $("#content").width();
-	var ideal_height = parseInt(($(window).height() - $(".navbar").height()) / 2) - 10;
+	//var ideal_height = parseInt(($(window).height() - $(".navbar").height()) / 2) - 10;
+	//var ideal_height = (($(window).height() - $(".navbar").height()) / 2) - 500;
+	var ideal_height = 120;
 	
 	var summed_width = _.reduce($imgs, function (memo, img) {
-		return $(img).width();
+		return parseFloat($(img).attr("data-original-width")) + memo;
 	}, 0);
 	
 	var rows = Math.round(summed_width / viewport_width);
@@ -106,19 +109,37 @@ var galleryify = function() {
 		});
 		
 		var partition = linear_partition(weights, rows);
+		var arr_partition = [];
+		var new_p = [];
+		var part_index = 0;
+		for(var i =0; i < partition.length; i++) {
+			if(partition[i] !== undefined || partition[i+1] !== undefined) {
+				new_p.push(partition[i]);
+			}
+		}
+		for(var i =0; i < new_p.length; i++) {
+			if (new_p[i]) {
+				if (arr_partition[part_index] === undefined) {
+					arr_partition[part_index] = [];
+				}
+				arr_partition[part_index].push(new_p[i]);
+			} else {
+				part_index++;
+			}
+		}
 		var index = 0;
 		var row_buffer = [];
-		_.each(partition, function(row) {
+		_.each(arr_partition, function(row) {
 			row_buffer = [];
 			_.each(row, function(im) {
 				row_buffer.push($imgs[index++]);
 			});
-				var summed_ratios = _.reduce(row_buffer, function(img){
-					return $img.data("aspectRatio");
+				var summed_ratios = _.reduce(row_buffer, function(memo, img){
+					return parseFloat($(img).attr("data-aspect-ratio")) + memo;
 				}, 0);
-				_.each(row_buffer, function(photo) {
-					var $img = $(photo);
-					resize_photo($img, (viewport_width/summed_ratios)*$img.data("aspectRatio"), viewport_width/summed_ratios);
+				_.each(row_buffer, function(img) {
+					var $img = $(img);
+					resize_img($img, (viewport_width/summed_ratios)*parseFloat($img.attr("data-aspect-ratio")), viewport_width/summed_ratios);
 				});
 		});
 	}
--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -1,7 +1,9 @@
 class GalleryController < ApplicationController
   def index
-	@posts = Post.where("PHOTO_FILE_NAME IS NOT NULL").order('created_at DESC')
+	@posts = Post.where("PHOTO_FILE_NAME IS NOT NULL")
 	@covers = Cover.all()
+	@photos = @posts + @covers
+	@photos.shuffle
 	respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @posts }
