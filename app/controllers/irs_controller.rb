93d7fb94ad60b1a9027846d75e757792ef923563
Andrew Svoboda <asvoboda@uwaterloo.ca>
2013-09-28 20:41:03 -0400

    updated irs time to feb 8th, 2015, which is apparently the correct date. WHO KNEW

--- app/controllers/irs_controller.rb
+++ app/controllers/irs_controller.rb
@@ -1,8 +1,8 @@
 class IrsController < ApplicationController
   def index
 	respond_to do |format|
-	  # Feb 14, 2015 TODO: is this correct?
-	  @irs = (Date.new(2015, 02, 14) - Date.today).to_i
+	  # Feb 8, 2015 TODO: is this correct?
+	  @irs = (Date.new(2015, 02, 8) - Date.today).to_i
       format.html # index.html.erb
       format.json { render json: @irs }
 	end
